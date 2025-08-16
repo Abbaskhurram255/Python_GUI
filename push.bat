80e6a6c1c66d6bc6eaf4e5a82fd648268fdc7d13
Ayesha Mehnaaz <39873704+Abbaskhurram255@users.noreply.github.com>
2025-08-16 18:35:51 +0500

    Update .

--- push.bat
+++ push.bat
@@ -1,5 +1,5 @@
 @echo off
-copy "KL_Py.py" "C:\Users\Adi\Documents\GitHub\hindCPlusPlus" && copy "simplegui.py" "C:\Users\Adi\Documents\GitHub\hindCPlusPlus" && copy "build.bat" "C:\Users\Adi\Documents\GitHub\hindCPlusPlus" && copy "requirements.txt" "C:\Users\Adi\Documents\GitHub\hindCPlusPlus" && xcopy "build" "C:\Users\Adi\Documents\GitHub\hindCPlusPlus\build" /s /e /i /h /c /y
+copy "KL_Py.py" "C:\Users\Adi\Documents\GitHub\hindCPlusPlus" && copy "simplegui.py" "C:\Users\Adi\Documents\GitHub\hindCPlusPlus" && copy "requirements.txt" "C:\Users\Adi\Documents\GitHub\hindCPlusPlus" && xcopy "build" "C:\Users\Adi\Documents\GitHub\hindCPlusPlus\build" /s /e /i /h /c /y
 echo Uploading...
 cd C:\Users\Adi\Documents\GitHub\hindCPlusPlus && git add . && git commit -m "Update KL_Py.py" && git push
 cd C:\Users\Adi\Downloads\Python_GUI && git add . && git commit -m "Update ." && git push && cd ..\
